209a50b759571c2cf7165f0c68f838e019f7290e
RicardoFiguera <ricardofiguera.c@gmail.com>
2025-07-19 14:04:41 +0200

    modificado boton de agregar board, tambien cambiada su ubicacion

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,7 +7,7 @@ export const Navbar = () => {
       <div
         className="navbar bg-base-100 bg-opacity-90 shadow-sm mb-12  py-5 rounded-box">
         <div className="flex-1">
-          <Link to="/" className="ml-6 text-blue-700 text-2xl md:text-5xl font-bold">
+          <Link to="/" className="ml-6 text-2xl md:text-5xl font-bold">
             My Trello Clon
           </Link>
         </div>
@@ -15,32 +15,7 @@ export const Navbar = () => {
           <div className="dropdown dropdown-end mr-6" >
                 <ThemeToggle />
           </div>
-          <div className="dropdown dropdown-end mr-6">
-            <div
-              tabIndex={0}
-              role="button"
-              className="avatar">
-              <div className="w-16 rounded-full">
-                <img
-                  alt="Tailwind CSS Navbar component"
-                  src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
-                />
-              </div>
-            </div>
-            <ul
-              tabIndex={0}
-              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
-              <li>
-                <Link to="/" className="text-xl justify-between">Profile</Link>
-              </li>
-              <li>
-                <Link to="/settings" className="text-xl justify-between">Settings</Link>
-              </li>
-              <li>
-                <Link to="/login" className="text-xl justify-between">Login</Link>
-              </li>
-            </ul>
-          </div>
+         
         </div>
       </div>
     </>
--- src/pages/PrincipalPage.tsx
+++ src/pages/PrincipalPage.tsx
@@ -8,10 +8,9 @@ export const PrincipalPage = () => {
   const [boards, setBoards] = useState<Board[]>([]);
   const [showCreateBoard, setShowCreateBoard] = useState(false);
 
-  const {getAllBoards} = useBoard();
+  const { getAllBoards } = useBoard();
 
- 
-  const fetchBoards = async() => {
+  const fetchBoards = async () => {
     try {
       const boardData = await getAllBoards();
       setBoards(boardData);
@@ -25,9 +24,8 @@ export const PrincipalPage = () => {
   }, []);
 
   const handleAddBoard = () => {
-     setShowCreateBoard((prev) => !prev);
+    setShowCreateBoard((prev) => !prev);
   };
-  
 
   return (
     <>
@@ -35,11 +33,6 @@ export const PrincipalPage = () => {
         <div className="card-body">
           <div className="flex justify-between items-center py-6 md:pl-4 md:pr-4">
             <h2 className="card-title text-2xl md:text-5xl">Mis tableros</h2>
-            <button
-              className="btn btn-outline btn-xs btn-primary text-xl py-6"
-              onClick={handleAddBoard}>
-              + Añadir tablero
-            </button>
           </div>
           <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
             {boards.map((board) => (
@@ -50,11 +43,21 @@ export const PrincipalPage = () => {
                 description={board.description}
               />
             ))}
+            <button
+              onClick={handleAddBoard}
+              className="board-card flex items-center justify-center bg-gray-100 text-gray-600 rounded-lg
+               hover:bg-gray-200 transition-colors duration-200 min-h-32 cursor-pointer shadow-md"
+              style={{ boxShadow: "none" }}>
+              <span className="text-2xl font-bold">+</span>
+              <span className="text-lg ml-2">Crear nuevo tablero</span>
+            </button>
           </div>
         </div>
       </div>
 
-      {showCreateBoard && <CreateBoard handleAddBoard={handleAddBoard} setBoards={setBoards} />}
+      {showCreateBoard && (
+        <CreateBoard handleAddBoard={handleAddBoard} setBoards={setBoards} />
+      )}
     </>
   );
 };
