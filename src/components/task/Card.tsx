71139a279200a56d17472feb1a882ca6a76f1d5b
RicardoFiguera <ricardofiguera.c@gmail.com>
2025-07-19 17:29:13 +0200

    agregado boton menu para editar y eliminar un tablero

--- src/components/task/Card.tsx
+++ src/components/task/Card.tsx
@@ -34,9 +34,6 @@ export const Card = ({ id, title }: CardProps) => {
               viewBox="0 0 24 24"
               fill="none"
               stroke="currentColor"
-              stroke-width="2"
-              stroke-linecap="round"
-              stroke-linejoin="round"
               className="lucide lucide-pencil-icon lucide-pencil">
               <path d="M21.174 6.812a1 1 0 0 0-3.986-3.987L3.842 16.174a2 2 0 0 0-.5.83l-1.321 4.352a.5.5 0 0 0 .623.622l4.353-1.32a2 2 0 0 0 .83-.497z" />
               <path d="m15 5 4 4" />
@@ -50,9 +47,6 @@ export const Card = ({ id, title }: CardProps) => {
               viewBox="0 0 24 24"
               fill="none"
               stroke="currentColor"
-              stroke-width="2"
-              stroke-linecap="round"
-              stroke-linejoin="round"
               className="lucide lucide-trash-icon lucide-trash">
               <path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6" />
               <path d="M3 6h18" />
--- src/pages/BoardPage.tsx
+++ src/pages/BoardPage.tsx
@@ -5,11 +5,7 @@ import { useEffect, useState } from "react";
 
 export const BoardPage = () => {
   const { selectedBoard } = useSelecedBoard();
-  const  navigate  = useNavigate();
-
-  if (!selectedBoard.Id) {
-    navigate("/");
-  }
+  const navigate = useNavigate();
 
   const [showCreateColumn, setShowCreateColumn] = useState(false);
 
@@ -20,56 +16,91 @@ export const BoardPage = () => {
   useEffect(() => {
     if (!selectedBoard?.Id) {
       navigate("/", { replace: true });
-    } 
+    }
   }, [selectedBoard, navigate]);
-  
+
+  const handleDeleteBoard = () => {
+    alert("Eliminar tablero");
+  };
+
+  const handleEditBoard = () => {
+    alert("Editar tablero");
+  };
+
+
 
   return (
     <div>
       <div className="card bg-base-100 shadow-sm min-h-[60vh] opacity-90">
         <div className="card-body">
-
-              <div className="flex flex-col md:flex-row md:justify-between md:items-center py-6 md:pl-4 md:pr-4">
-                <div className="flex justify-between items-center w-full mb-4 md:mb-0">
-                  <Link
-                    to={"/"}
-                    className="flex items-center">
-                    <h2 className="card-title text-2xl md:text-3xl font-bold flex items-center gap-2">
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="30"
-                        height="30"
-                        viewBox="0 0 24 24"
-                        fill="none"
-                        stroke="currentColor"
-                        strokeWidth="2"
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        className="lucide lucide-arrow-left-icon lucide-arrow-left">
-                        <path d="m12 19-7-7 7-7" />
-                        <path d="M19 12H5" />
-                      </svg>
-                      Tableros
-                    </h2>
-                  </Link>
-                  <button
-                    onClick={handleAddColumn}
-                    className="btn btn-outline btn-xs text-xl py-6 rounded-lg px-4" >
-                    + Lista
-                  </button>
-                </div>
-                </div>
-
-                <h2 className="card-title text-3xl md:text-5xl font-bold w-full text-center mt-4 md:w-auto md:text-left md:mt-0">
-                  {selectedBoard.Title}
+          <div className="flex flex-col md:flex-row md:justify-between md:items-center py-6 md:pl-4 md:pr-4">
+            <div className="flex justify-between items-center w-full mb-4 md:mb-0">
+              <Link to={"/"} className="flex items-center">
+                <h2 className="card-title text-2xl md:text-3xl font-bold flex items-center gap-2">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="30"
+                    height="30"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    className="lucide lucide-arrow-left-icon lucide-arrow-left">
+                    <path d="m12 19-7-7 7-7" />
+                    <path d="M19 12H5" />
+                  </svg>
+                  Tableros
                 </h2>
-              
-              <div className="overflow-x-auto">
-                <Board
-                  showCreateColumn={showCreateColumn}
-                  handleAddColumn={handleAddColumn}
-                />
+              </Link>
+              <div className="flex items-center gap-4">
+                 <button
+                  onClick={handleAddColumn}
+                  className="btn btn-outline btn-xs text-xl py-6 rounded-lg px-4">
+                  + Lista
+                </button>
+                <div className="dropdown dropdown-end">
+                  <div
+                    tabIndex={0}
+                    role="button"
+                    className="btn btn-outline m-1 btn-xs text-xl py-6 rounded-lg px-4">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="24"
+                      height="24"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      stroke="currentColor"
+                      className="lucide lucide-menu-icon lucide-menu">
+                      <path d="M4 12h16" />
+                      <path d="M4 18h16" />
+                      <path d="M4 6h16" />
+                    </svg>
+                  </div>
+                  <ul
+                    tabIndex={0}
+                    className="dropdown-content menu bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm">
+                    <li>
+                      <button className="" onClick={handleEditBoard}>Editar tablero</button>
+                    </li>
+                    <li>
+                      <button className="text-error" onClick={handleDeleteBoard}>Eliminar tablero</button>
+                    </li>
+                  </ul>
+                </div>
+               
               </div>
+            </div>
+          </div>
+
+          <h2 className="card-title text-3xl md:text-5xl font-bold w-full text-center mt-4 md:w-auto md:text-left md:mt-0">
+            {selectedBoard.Title}
+          </h2>
+
+          <div className="overflow-x-auto">
+            <Board
+              showCreateColumn={showCreateColumn}
+              handleAddColumn={handleAddColumn}
+            />
+          </div>
         </div>
       </div>
     </div>
